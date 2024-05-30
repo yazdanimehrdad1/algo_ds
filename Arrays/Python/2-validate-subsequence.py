8a7bf3206d9f92a4898ac7a62c6e3b533eb84a99
yazdanibiouki <mehrdad.yazdanibiouki@powerflex.com>
2024-05-30 10:08:28 -0700

    array-3

--- Arrays/Python/2-validate-subsequence.py
+++ Arrays/Python/2-validate-subsequence.py
@@ -1,4 +1,9 @@
 """
+Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array. 
+For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4] , and so do the numbers [2, 4] . 
+Note that a single number in an array and the array itself are both valid subsequences of the array.
+
 Input: 
 array:[5,1,22,25,6,-1, 8,10]
 sequence:[1, 6 , -1, 10]
@@ -11,7 +16,7 @@ O(n) time | O(1) space
 def isValidSubsequence(array,subseq):
     arrIdx = 0
     seqIdx = 0
-    while arrIdx < len(array):
+    while arrIdx < len(array) and seqIdx < len(seqIdx):
         if array[arrIdx] == subseq[seqIdx]:
             seqIdx +=1
         arrIdx+=1
@@ -28,3 +33,31 @@ def isValidSubsequence(array, sequence):
         if seqIdx == len(sequence):
             break
     return seqIdx == len(sequence)
+
+
+#Practice1
+array= [5,1,22,25,6,-1, 8,10]
+sequence=[1, 6 , -1, 10]
+def practice1(array, sequence):
+    seqIdx = 0
+    for arrIdx in range(len(array)):
+        arrValue = array[arrIdx]
+        if arrValue == sequence[seqIdx]:
+            seqIdx +=1
+    return seqIdx == len(sequence)
+
+def practice2(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx +=1
+        arrIdx +=1
+    return seqIdx == len(sequence)
+
+
+
+array:[5,1,22,25,6,-1, 8,10]
+sequence:[1, 6 , -1, 10]
+result1 = practice1(array, sequence)
+print(result1)
--- Arrays/Python/3-sorted-square-array.py
+++ Arrays/Python/3-sorted-square-array.py
@@ -1,4 +1,7 @@
+# Write a function that takes in a non-empty array of integers that are sorted in ascending order and 
+# returns a new array of the same length with the squares of the original integers also sorted in ascending order.
 # input is sorted array of integers but keep in mind that they could be negative too
+
 """
 input = [1, 2, 3, 5, 6, 8, 9]
 expected = [1, 4, 9, 25, 36, 64, 81]
@@ -23,7 +26,6 @@ def sortedSquaredArray(array):
     smallerValIdx = 0
     largerValIdx = len(array)-1
     for i in range(len(array)):
-        print(len(array)-1-i)
         smallerVal = array[smallerValIdx]
         largerVal = array[largerValIdx]
         # print([smallerVal, largerVal])
@@ -38,4 +40,28 @@ def sortedSquaredArray(array):
 
 inputArr = [-10, 2, 3, 5, 6, 8, 9]
 result = sortedSquaredArray(inputArr)
-print(result)
+# print(result)
+
+
+# practice
+def practice1(array):
+    newArr = [ 0 for _ in array]
+    left = 0
+    right = len(array) -1
+    for i in reversed(range(len(array))):
+
+
+        absLeftVal = abs(array[left])
+        absRightVal = abs(array[right])
+        print(absLeftVal,absRightVal)
+
+        if absLeftVal > absRightVal:
+            newArr[i] = absLeftVal*absLeftVal
+            left +=1
+        else:
+            newArr[i] = absRightVal * absRightVal
+            right -=1
+    return newArr
+input = [-10, -5, 0, 5, 10]
+result1 = practice1(input)
+print(result1)
